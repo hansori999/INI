08fa6c42bb20cf357eed354c970fb66f952971b3
Myoung-Wan Koo <mwkoo9@gmail.com>
2016-05-11 12:40:23 +0900

    Initial Commit

---
+++ Initialization/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Initialization
+//
+//  Created by Myoung-Wan Koo on 2016. 5. 11..
+//  Copyright © 2016년 Myoung-Wan Koo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
